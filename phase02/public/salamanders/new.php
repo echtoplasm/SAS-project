e411015f7148f102979fb10fb611b4dcac7b9069
echtoplasm <zacharyamassey@gmail.com>
2025-03-07 12:00:15 -0500

    added form to new

--- phase02/public/salamanders/new.php
+++ phase02/public/salamanders/new.php
@@ -20,9 +20,18 @@ switch($test) {
   case 'redirect_external':
     redirect_to('http://www.google.com');
     break;
-
-  default:
-    echo 'No error';
-    break;
 }
 ?>
+
+<?php $page_title = 'Create Salamander'; ?>
+
+<?php include(SHARED_PATH . '/salamander-header.php'); ?>
+
+<h1>Create Salamander</h1>
+<form action="/sas/phase02/public/salamanders/create.php" method="post">
+    <label for="salamanderName">Name</label><br>
+    <input type="text" name="salamanderName"/><br>
+    <input type="submit" value="Create Salamander"/>
+</form>
+
+<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
